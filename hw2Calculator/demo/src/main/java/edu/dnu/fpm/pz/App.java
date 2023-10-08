304a767956e07b16d5eb547d210641a0f2901546
nfx 749 <nfx749@gmail.com>
2023-10-08 17:01:58 +0000

    fixed code

--- hw2Calculator/demo/src/main/java/edu/dnu/fpm/pz/App.java
+++ hw2Calculator/demo/src/main/java/edu/dnu/fpm/pz/App.java
@@ -1,6 +1,6 @@
 package edu.dnu.fpm.pz;
 
-public class App {
+public class CalculatorApp {
     public static void main(String[] args) {
         if (args.length < 3) {
             System.err.println("Not enough parameters!");
@@ -8,42 +8,40 @@ public class App {
         }
 
         Calc calc = new CalcImpl();
-        double a;
+        double firstNumber;
+        double secondNumber;
         try {
-            a = Double.parseDouble(args[0]);
-        } catch (Exception e) {
-            System.err.println("Invalid first argument!");
+            firstNumber = Double.parseDouble(args[0]);
+            secondNumber = Double.parseDouble(args[1]);
+        } catch (NumberFormatException e) {
+            System.err.println("Invalid number format!");
             return;
         }
 
-        double b;
-        try {
-            b = Double.parseDouble(args[1]);
-        } catch (Exception e) {
-            System.err.println("Invalid second argument!");
-            return;
-        }
-
-        double result = 0;
+        double result;
         switch (args[2]) {
             case "+":
-                result = calc.addition(a, b);
+                result = calc.addition(firstNumber, secondNumber);
                 break;
             case "-":
-                result = calc.subtraction(a, b);
+                result = calc.subtraction(firstNumber, secondNumber);
                 break;
             case "*":
-                result = calc.multiplication(a, b);
+                result = calc.multiplication(firstNumber, secondNumber);
                 break;
             case "/":
-                result = calc.division(a, b);
+                if (secondNumber == 0) {
+                    System.err.println("Division by zero!");
+                    return;
+                }
+                result = calc.division(firstNumber, secondNumber);
                 break;
             default:
                 System.err.println("Invalid operator!");
                 return;
         }
 
-        System.out.println("number1= " + a + " number2= " + b +
-                " operator= " + args[2] + " result= " + result);
+        System.out.println("Number1 = " + firstNumber + " Number2 = " + secondNumber +
+                " Operator = " + args[2] + " Result = " + result);
     }
 }
