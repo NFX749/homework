97a8f3059eb14163ac381bdd455dc4f3e1eb56b4
nfx 749 <nfx749@gmail.com>
2023-10-08 19:53:38 +0000

    fixed code

--- hw2Calculator/calc-interface/src/main/java/edu/dnu/fpm/pz/Calc.java
+++ hw2Calculator/calc-interface/src/main/java/edu/dnu/fpm/pz/Calc.java
@@ -8,36 +8,36 @@ public interface Calc {
     /**
      * Adds two numbers.
      *
-     * @param a The first number.
-     * @param b The second number.
+     * @param firstParam The first number.
+     * @param secondParam The second number.
      * @return The result of addition.
      */
-    double addition(double a, double b);
+    double addition(double firstParam, double secondParam);
 
     /**
      * Subtracts two numbers.
      *
-     * @param a The first number.
-     * @param b The second number.
+     * @param firstParam The first number.
+     * @param secondParam The second number.
      * @return The result of subtraction.
      */
-    double subtraction(double a, double b);
+    double subtraction(double firstParam, double secondParam);
 
     /**
      * Multiplies two numbers.
      *
-     * @param a The first number.
-     * @param b The second number.
+     * @param firstParam The first number.
+     * @param secondParam The second number.
      * @return The result of multiplication.
      */
-    double multiplication(double a, double b);
+    double multiplication(double firstParam, double secondParam);
 
     /**
      * Divides two numbers.
      *
-     * @param a The dividend.
-     * @param b The divisor (must not be zero).
+     * @param firstParam The dividend.
+     * @param secondParam The divisor (must not be zero).
      * @return The result of division.
      */
-    double division(double a, double b);
+    double division(double firstParam, double secondParam);
 }
